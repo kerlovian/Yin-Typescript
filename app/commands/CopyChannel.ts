c246505ed0cfce29ad7979835089367067d8f4a4
kerlovian <52460899+kerlovian@users.noreply.github.com>
2019-10-06 09:26:43 -0400

    CopyChannel command

---
+++ app/commands/CopyChannel.ts
@@ -0,0 +1,36 @@
+import Command from "../Command";
+import BotClient from "../BotClient";
+
+import { Message } from "discord.js";
+
+
+export default class CopyChannel extends Command {
+    public name: string = "copychannel";
+
+
+    public constructor (client: BotClient) {
+        super(client);
+    }
+
+    public checkPermissions (message: Message): boolean {
+        return message.author!.id === this.client.config.ownerID;
+    }
+
+    public async run (message: Message, args: string[]) {
+        const [baseChannel, AS, type, TO, ...name] = args;
+
+        const ch = message.guild!.channels.resolve(baseChannel);
+        if (!ch) return Command.fail(message, "base channel not found");
+
+        if (!type || !["category", "text", "voice"].includes(type)) return Command.fail(message, "invalid channel type");
+        if (!name) return Command.fail(message, "no specified name");
+
+        const perms = ch.permissionOverwrites;
+        const createdChannel = await message.guild!.channels.create(name.join("-"), {
+            type: type as "category" || "text" || "voice",
+            permissionOverwrites: perms
+        });
+
+        return Command.success(message, `Channel <#${createdChannel.id}> created`);
+    }
+}
