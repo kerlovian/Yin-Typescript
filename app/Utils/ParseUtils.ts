48d17da7961ebb86bc36f334f3e6b13715ef0feb
kerlovian <52460899+kerlovian@users.noreply.github.com>
2019-10-25 21:25:24 -0400

    Fixed sequential matching problem

--- app/Utils/ParseUtils.ts
+++ app/Utils/ParseUtils.ts
@@ -1,39 +1,43 @@
 export default abstract class ParseUtils {
     //matches emoji ID from raw emoji mention (string) or just the ID by itself
-    public static readonly EMOJI_REGEXP = /(?:(?:<:\w{2,32}:)(\d{17,19})(?:>))|(\d{17,19})/g;
+    public static readonly EMOJI_REGEXP = /(?:(?:<:\w{2,32}:)(?<fromMention>\d{17,19})(?:>))|(?<byItself>\d{17,19})/g;
 
     public static MATCH_EMOJI (raw: string): string {
-        const match = raw.match(this.EMOJI_REGEXP)!;
-        return match[0];
+        this.EMOJI_REGEXP.lastIndex = 0; //prevents successive matching
+        const match = this.EMOJI_REGEXP.exec(raw)!.groups!;
+        return match["fromMention"] || match["byItself"];
     }
 
 
 
     //matches user ID from raw user mention (string) or just the ID by itself
-    public static readonly USER_REGEXP = /(?:<@!?(\d{17,19})>)|(\d{17,19})/g;
+    public static readonly USER_REGEXP = /(?:<@!?(?<fromMention>\d{17,19})>)|(?<byItself>\d{17,19})/g;
 
     public static MATCH_USER (raw: string): string {
-        const match = raw.match(this.USER_REGEXP)!;
-        return match[0];
+        this.USER_REGEXP.lastIndex = 0; //prevents successive matching
+        const match = this.USER_REGEXP.exec(raw)!.groups!;
+        return match["fromMention"] || match["byItself"];
     }
 
 
 
     //matches role ID from raw role mention (string) or just the ID by itself
-    public static readonly ROLE_REGEXP = /(?:<@&(\d{17,19})>)|(\d{17,19})/g;
+    public static readonly ROLE_REGEXP = /(?:<@&(?<fromMention>\d{17,19})>)|(?<byItself>\d{17,19})/g;
 
     public static MATCH_ROLE (raw: string): string {
-        const match = raw.match(this.ROLE_REGEXP)!;
-        return match[0];
+        this.ROLE_REGEXP.lastIndex = 0; //prevents successive matching
+        const match = this.ROLE_REGEXP.exec(raw)!.groups!;
+        return match["fromMention"] || match["byItself"];
     }
 
 
 
     //matches channel ID from raw channel mention (string) or just the ID by itself
-    public static readonly CHANNEL_REGEXP = /(?:<#(\d{17,19})>)|(\d{17,19})/g;
+    public static readonly CHANNEL_REGEXP = /(?:<#(?<fromMention>\d{17,19})>)|(?<byItself>\d{17,19})/g;
 
     public static MATCH_CHANNEL (raw: string): string {
-        const match = raw.match(this.CHANNEL_REGEXP)!;
-        return match[0];
+        this.CHANNEL_REGEXP.lastIndex = 0; //prevents successive matching
+        const match = this.CHANNEL_REGEXP.exec(raw)!.groups!;
+        return match["fromMention"] || match["byItself"];
     }
 }
